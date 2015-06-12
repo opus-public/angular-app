d838f4c0557721efeb4d43058aa0eb7449742d84
Darragh Flynn <Darragh.Flynn@realexpayments.com>
2015-06-12 13:30:07 +0100

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opusSolutionsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the opusSolutionsApp
+ */
+angular.module('opusSolutionsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
